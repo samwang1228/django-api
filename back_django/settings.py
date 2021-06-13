c4d04cdf3a22a333d1fbb01aa85c49b0b24d1594
samwang1228 <samwang19991228@gmail.com>
2021-06-13 16:22:33 +0800

    heroku

--- back_django/settings.py
+++ back_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!5l-m&+x+#!(5zhn8f!o_6*$m4c77=cfnmjd*t2k(m0%4rfp(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','final-project-webprograming.herokuapp.com']
 
 
 # Application definition
